21115c96b6353e8511b2518e556f055806b51c92
satyaj2se <49920876+satyaj2se@users.noreply.github.com>
2022-02-14 15:40:30 +0530

    Update excelHelp.java

--- excelHelp.java
+++ excelHelp.java
@@ -1,3 +1,9 @@
 if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
     String str = NumberToTextConverter.toText(cell.getNumericCellValue())
 }
+
+ // Only need one of these
+DataFormatter fmt = new DataFormatter();
+
+// Once per cell
+String valueAsSeenInExcel = fmt.formatCellValue(cell);
