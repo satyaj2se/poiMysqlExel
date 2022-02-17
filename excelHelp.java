bb6214a42c51e5f8e974c49f683a3e499fe16d37
satyaj2se <49920876+satyaj2se@users.noreply.github.com>
2022-02-17 13:01:41 +0530

    Update excelHelp.java

--- excelHelp.java
+++ excelHelp.java
@@ -7,3 +7,14 @@ DataFormatter fmt = new DataFormatter();
 
 // Once per cell
 String valueAsSeenInExcel = fmt.formatCellValue(cell);
+
+public static boolean isColumnEmpty(Sheet sheet, int columnIndex, int startRow) {
+    for (Row row : sheet) {
+        if (row.getRowNum() < startRow) continue;
+        Cell cell = row.getCell(columnIndex, Row.RETURN_BLANK_AS_NULL);
+        if (cell != null) {
+            return false;
+        }
+    }
+    return true;
+}
