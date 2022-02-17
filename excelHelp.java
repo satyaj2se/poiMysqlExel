fd657dc27e38bbe9ba4abb7fc8473fa132b0b6d2
satyaj2se <49920876+satyaj2se@users.noreply.github.com>
2022-02-17 14:12:14 +0530

    Update excelHelp.java

--- excelHelp.java
+++ excelHelp.java
@@ -1,20 +1,4 @@
-if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
-    String str = NumberToTextConverter.toText(cell.getNumericCellValue())
-}
-
- // Only need one of these
-DataFormatter fmt = new DataFormatter();
-
-// Once per cell
-String valueAsSeenInExcel = fmt.formatCellValue(cell);
-
-public static boolean isColumnEmpty(Sheet sheet, int columnIndex, int startRow) {
-    for (Row row : sheet) {
-        if (row.getRowNum() < startRow) continue;
-        Cell cell = row.getCell(columnIndex, Row.RETURN_BLANK_AS_NULL);
-        if (cell != null) {
-            return false;
-        }
-    }
-    return true;
-}
+Customer james = customers.stream()
+  .filter(customer -> "James".equals(customer.getName()))
+  .findAny()
+  .orElse(null);
