7c68d0886fb29e5cfa4421516027695934d7de4e
satyaj2se <49920876+satyaj2se@users.noreply.github.com>
2022-03-03 21:15:55 +0530

    Update excelHelp.java

--- excelHelp.java
+++ excelHelp.java
@@ -2,3 +2,6 @@ Customer james = customers.stream()
   .filter(customer -> "James".equals(customer.getName()))
   .findAny()
   .orElse(null);
+
+
+java.lang.ClassCastException: com.ibm.xml.xlxp2.api.stax.XMLEventFactoryImpl incompatible with javax.xml.stream.XMLEventFactory
